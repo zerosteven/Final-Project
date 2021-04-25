65519df526e84fe107be2b9f733249a0b52306e4
Bohan Guo <bohanguo@umich.edu>
2021-04-25 23:31:12 +0800

    start

--- draw_data.py
+++ draw_data.py
@@ -1,2 +1,25 @@
 from matplotlib import pyplot as plt
-print('Hello World')
+import json
+import os
+
+
+
+def read_cache(self, CACHE_FNAME):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    CACHE_FILE = dir_path + '/' + CACHE_FNAME + ".json"
+    try:
+        cache_file = open(CACHE_FILE, 'r', encoding="utf-8")
+        cache_contents = cache_file.read()
+        cache_list = json.loads(cache_contents)
+        cache_file.close()
+        return cache_list
+    except:
+        cache_list = {}
+        return cache_list
+
+
+def write_cache(self, cache_file, cache_dict):       
+        dumped_json_cache = json.dumps(cache_dict)
+        f = open(cache_file + ".json", 'w')
+        f.write(dumped_json_cache)
+        f.close()

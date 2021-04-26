c454ae1dfd6f9abc810bdb49ff8aa70e8d4e5be0
Bohan Guo <bohanguo@umich.edu>
2021-04-26 20:36:26 +0800

    databasebuilder

--- process_data.py
+++ process_data.py
@@ -4,12 +4,14 @@ import os
 import numpy as np
 from matplotlib import pyplot as plt
 from sqlite3.dbapi2 import SQLITE_SELECT
+from cache import cache
 
 class process_data:
     
 
     def __init__(self, db_name, table1, table2,table3,table4):
         
+        self.cache = cache()
         self.db_name = db_name
         self.table1 = table1
         self.table2 = table2
@@ -24,9 +26,9 @@ class process_data:
 
     def SetUpTable(self, cachefile1, cachefile2, cachefile3, cur, conn):
 
-        temp = self.Cache.read_cache(cachefile1)
-        air_quality = self.Cache.read_cache(cachefile2)
-        chance_of_rain = self.Cache.read_cache(cachefile3)
+        temp_data = self.cache.read_cache(cachefile1)
+        air_quality_data = self.cache.read_cache(cachefile2)
+        chance_of_rain_data = self.cache.read_cache(cachefile3)
         cur.execute(
             f"CREATE TABLE IF NOT EXISTS {self.table1} (id TEXT PRIMARY KEY, temp INTEGER)")
         cur.execute(
@@ -35,3 +37,8 @@ class process_data:
             f"CREATE TABLE IF NOT EXISTS {self.table3} (id TEXT PRIMARY KEY, )")
         cur.execute(
             f"CREATE TABLE IF NOT EXISTS {self.table4} (id TEXT PRIMARY KEY, )")
+        for item in temp_data:
+            id = item
+            temp = temp_data[item]
+            cur.execute(
+                f"INSERT OR IGNORE INTO {self.table1} (id, temp) VALUES (?,?)", (id, temp))    

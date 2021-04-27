5a1fe60d3738a6ffc5720b9acc549775185f71f4
Bohan Guo <bohanguo@umich.edu>
2021-04-28 00:24:01 +0800

    finished

--- process_data.py
+++ process_data.py
@@ -65,3 +65,62 @@ class process_data:
         cur.execute('DELETE FROM humidity_data WHERE humidity_data.id IN (SELECT humidity_data.id FROM humidity_data JOIN temp_data ON temp_data.id = humidity_data.id WHERE temp_data.temperature <= 0)')
         conn.commit()
 
+
+    def correlation_of_data(self, col1, col2,table1,table2, cur, conn):
+        
+        # get all data from specfic column and fetch it all 
+        cur.execute(f'SELECT {col1} FROM {table1}')
+        x = cur.fetchall()
+        l0 = list()
+        for item in x:
+            l0.append(item[0])
+        
+        # we need to conver list into np array in order to use correlation matrix to do it 
+        l0 = np.array(l0)
+
+        #get data from antoher column
+        cur.execute(f'SELECT {col2} FROM {table2}')
+        y = cur.fetchall()
+        l1 = list()
+        for item in y:
+            l1.append(item[0])
+        l1 = np.array(l1)
+        
+        conn.commit()
+        r = np.corrcoef(l0, l1)
+        return r[0,1]
+        #[i,j] refers to the element in the i-th row and j-th column
+        #[0,1] here refers to the correlation between first array and second arrary
+    
+    def visualization(self, col1, table1, col2, table2, cur, conn, x_label, y_label, title):  
+        #scatter plot!
+    
+        cur.execute(f'SELECT {col1} FROM {table1}')
+        x1 = cur.fetchall()
+        l0 = list()
+        for item in x:
+            l0.append(item[0])
+
+        cur.execute(f'SELECT {col1} FROM {tb1}')
+        x2 = cur.fetchall()
+        l1 = list()
+        for item in y:
+            l1.append(item[0])
+        conn.commit()
+
+        #draw the scatter plot
+        x1 = np.array(l0)
+        x2 = np.array(l1)
+        plt.scatter(l0, l1)
+        #np.polyfit method is used to fit our data inside a polynomial function.
+        #with degree of 1
+        m, b = np.polyfit(x1, x2, 1)
+        plt.plot(x1, m * x1 + b, 'r')
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.title(title)
+        plt.show()
+    
+    def explanatory_file(self, filename, input):
+        with open(filename,'a') as f:
+            f.write(input + '\n')
